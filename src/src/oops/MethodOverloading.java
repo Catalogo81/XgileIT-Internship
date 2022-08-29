819c113eb9b04d879007dc1b4c9ca4c7795d8829
Catalogo81 <peacematjato@gmail.com>
2022-08-29 08:59:19 +0200

    MethodOverloading added

--- src/src/oops/MethodOverloading.java
+++ src/src/oops/MethodOverloading.java
@@ -1,4 +1,33 @@
 package oops;
 
-public class MethodOverloading {
+//process of creating multiple methods if the same name in the same class, and all methods work in different ways
+public class MethodOverloading
+{
+    public static void main(String[] args) {
+        MyShapes myShapes = new MyShapes();
+
+        myShapes.area(); //any area
+        myShapes.area(5); //circle area
+        myShapes.area(6.4, 1.45); //triangle area
+    }
+}
+
+class MyShapes
+{
+    public void area()
+    {
+        System.out.println("Formula for area of a shape is...");
+    }
+
+    public void area(int r) // radius
+    {
+        //area = 3.14 * r * r
+        System.out.println("Formula for area of a Circle is 3.14 times radius squared");
+    }
+
+    public void area(double b, double h) //breath, height
+    {
+        //area = 0.5 * b * h
+        System.out.println("Formula for area of a Triangle is half base times height");
+    }
 }
