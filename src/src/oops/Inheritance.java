1c1a1b48558c44f1680a0de1b46a6172e224580d
Catalogo81 <peacematjato@gmail.com>
2022-08-29 08:42:03 +0200

    Inheritance added

--- src/src/oops/Inheritance.java
+++ src/src/oops/Inheritance.java
@@ -1,4 +1,43 @@
 package oops;
 
-public class Inheritance {
+//Inheriting properties and behaviours from parent
+public class Inheritance
+{
+    public static void main(String[] args) //has-a
+    {
+        CalcVeryAdv c1 = new CalcVeryAdv(); //creating a calculator object
+        int sumResult = c1.add(4, 5);
+        int minusResult = c1.minus(4, 5);
+        int multiResult = c1.multi(4, 5);
+
+        System.out.println("Answer: " + sumResult);
+        System.out.println("Answer: " + minusResult);
+        System.out.println("Answer: " + multiResult);
+
+    }
 }
+
+class Calculator //parent, super class
+{
+    public int add(int i, int j)
+    {
+        return i + j;
+    }
+}
+
+class CalcAdv extends Calculator //child class, is-a
+{
+    public int minus(int i, int j)
+    {
+        return i - j;
+    }
+}
+class CalcVeryAdv extends CalcAdv //child class, is-a
+{
+    public int multi(int i, int j)
+    {
+        return i * j;
+    }
+}
+
+
--- src/src/oops/Polymorphism.java
+++ src/src/oops/Polymorphism.java
@@ -1,4 +1,7 @@
 package oops;
 
+/*ability for an object to take in different forms
+when a class has the ability to provide different implementations of a method, depending on the type of object passed
+to method */
 public class Polymorphism {
 }
