fb09946c4095e88a42d4d36b9b0aa08b1624c998
Catalogo81 <peacematjato@gmail.com>
2022-08-29 02:24:13 +0200

    Encapsulation added

--- src/src/oops/Encapsulation.java
+++ src/src/oops/Encapsulation.java
@@ -1,4 +1,44 @@
 package oops;
 
-public class Encapsulation {
+//binding data with methods
+public class Encapsulation
+{
+    public static void main(String[] args) {
+
+        //create a student
+        Student s1 = new Student();
+        s1.setName("Kgotso");
+        s1.setRollNum(6);
+
+        //display
+        System.out.println("Name: " + s1.getName());
+        System.out.println("RollNum: " + s1.getRollNum());
+    }
+
 }
+
+class Student {
+
+    //variables
+    private int rollNum;
+    private String name;
+
+    //Getters and Setters
+
+    public int getRollNum() {
+        return rollNum;
+    }
+
+    public void setRollNum(int rollNum) {
+        this.rollNum = rollNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
