ef867a7a5c0c660b5a12ef07ff3088e1cd60e1ec
Catalogo81 <peacematjato@gmail.com>
2022-08-29 08:50:23 +0200

    Polymorphism added

--- src/src/oops/Polymorphism.java
+++ src/src/oops/Polymorphism.java
@@ -3,5 +3,40 @@ package oops;
 /*ability for an object to take in different forms
 when a class has the ability to provide different implementations of a method, depending on the type of object passed
 to method */
-public class Polymorphism {
+public class Polymorphism
+{
+    public static void main(String[] args) {
+        Shapes myShape = new Shapes(); //ordinary shape object
+        Shapes myTriangle = new Triangle(); //triangle shape object
+        Shapes myCircle = new Circle(); //circle shape object
+
+        /* call the objects and its methods */
+        myShape.area();
+        myTriangle.area();
+        myCircle.area();
+    }
+}
+
+class Shapes
+{
+    public void area()
+    {
+        System.out.println("Formula for area of a shape is...");
+    }
+}
+
+class Triangle extends Shapes
+{
+    public void area()
+    {
+        System.out.println("Formula for area of a Triangle is half base times height");
+    }
+}
+
+class Circle extends Shapes
+{
+    public void area()
+    {
+        System.out.println("Formula for area of a Circle is 3.14 times radius squared");
+    }
 }
