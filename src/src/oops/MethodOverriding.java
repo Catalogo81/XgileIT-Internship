a3cf26f3a082b15f175b4e60f6c4459f63a249fc
Catalogo81 <peacematjato@gmail.com>
2022-08-29 09:21:20 +0200

    MethodOverloading added

--- src/src/oops/MethodOverriding.java
+++ src/src/oops/MethodOverriding.java
@@ -1,4 +1,28 @@
 package oops;
 
-public class MethodOverriding {
+//The process when a child class has the same method declared in the parent class
+public class MethodOverriding
+{
+    public static void main(String[] args)
+    {
+        Car car = new Car(); //car2 object created
+
+        car.run(); //calling from the child class
+    }
+}
+
+class Vehicle //parent
+{
+    void run()
+    {
+        System.out.println("Vehicle is running");
+    }
+}
+
+class Car extends Vehicle//child
+{
+    void run()
+    {
+        System.out.println("car is running safely");
+    }
 }
